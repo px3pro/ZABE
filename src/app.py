b9cc749250fee7d677f9df22b05c07ec91ec95d6
px3pro <ucurely@gmail.com>
2025-06-30 11:08:39 +0000

    Commit all workspace code for AWS deployment preparation

---
+++ src/app.py
@@ -0,0 +1,25 @@
+import sys
+import os
+from flask import Flask, render_template
+
+# Add the src directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import main as main_module  # Import from src directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def show_dashboard():
+    # Get data from main.py
+    output = []
+    import io
+    import sys
+    sys.stdout = io.StringIO()
+    main_module.orchestrate_zabe()
+    output = sys.stdout.getvalue().split('\n')
+    sys.stdout = sys.__stdout__
+    return render_template('dashboard.html', outputs=output)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
